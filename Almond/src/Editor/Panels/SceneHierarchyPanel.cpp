666dfab46917288e7bd87818dada0cc16d0b9e6a
Huzaifa Ahmed <huzaifaahmed12@gmail.com>
2021-09-30 23:24:23 +0100

    Entity Creation/Deletion and Adding/Removing components UI

--- Almond/src/Editor/Panels/SceneHierarchyPanel.cpp
+++ Almond/src/Editor/Panels/SceneHierarchyPanel.cpp
@@ -18,16 +18,50 @@ SceneHierarchyPanel::SceneHierarchyPanel(const std::shared_ptr<Scene> scene)
 void SceneHierarchyPanel::OnImGuiRender()
 {
 	ImGui::Begin("Scene Hierarchy");
-
-	for (auto ent : SceneView<Transform>(m_Scene->m_Ecs))
+	for (auto ent : SceneView<>(m_Scene->m_Ecs))
 	{
 		Entity entity{ent, m_Scene.get()};
 		DrawEntityNode(entity);
+	}
+
+	if (ImGui::BeginPopupContextWindow(0,1,false))
+	{
+		if (ImGui::MenuItem("Create Empty Entity"))
+		{
+			m_Scene->CreateEntity("Empty Entity");
+		}
+		ImGui::EndPopup();
 	}
 	ImGui::End();
 
 	ImGui::Begin("Properties");
 	DrawEntityProperties(m_SelectedEntity);
+
+	if (m_SelectedEntity.IsValid())
+	{
+		std::string button = "Add Component";
+		float font_size = ImGui::GetFontSize() * button.size() / 2;
+		ImGui::Spacing();
+		ImGui::SameLine(
+			ImGui::GetContentRegionAvail().x / 2 -
+			font_size + (font_size / 2)
+		);
+		
+		if (ImGui::Button(button.c_str()))
+			ImGui::OpenPopup("Add Component");
+		
+		if (ImGui::BeginPopup("Add Component"))
+		{
+			if (ImGui::MenuItem("Sprite Renderer"))
+			{
+				m_SelectedEntity.AddComponent<SpriteRender>({ 50.0f,50.0f, {1,1,1,1} });
+				ImGui::CloseCurrentPopup();
+			}
+			ImGui::EndPopup();
+		}
+	}
+	
+	
 	ImGui::End();
 	
 	ImGui::ShowDemoWindow();
@@ -39,17 +73,36 @@ static void DrawComponent(const std::string& name, Entity entity, UIFunction uiF
 	const ImGuiTreeNodeFlags treeNodeFlags = ImGuiTreeNodeFlags_DefaultOpen | ImGuiTreeNodeFlags_Framed | ImGuiTreeNodeFlags_SpanAvailWidth | ImGuiTreeNodeFlags_AllowItemOverlap | ImGuiTreeNodeFlags_FramePadding;
 	if (entity.HasComponent<T>())
 	{
+		auto io = ImGui::GetIO();
+		auto boldFont = io.Fonts->Fonts[0];
 		auto component = entity.GetComponent<T>();
 
 		ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, { 0,3 });
+		ImGui::PushFont(boldFont);
 		bool open = ImGui::TreeNodeEx((void*)typeid(T).hash_code(), treeNodeFlags, name.c_str());
+		ImGui::PopFont();
 		ImGui::PopStyleVar();
+
+		bool componentRemoved = false;
+		if (ImGui::BeginPopupContextItem(0))
+		{
+			if (ImGui::MenuItem("Remove Component"))
+			{
+				componentRemoved = true;
+			}
+			ImGui::EndPopup();
+		}
 		
 		if (open)
 		{
 			uiFunction(component);
 			ImGui::Separator();
 			ImGui::TreePop();
+		}
+
+		if (componentRemoved)
+		{
+			entity.RemoveComponent(component);
 		}
 	}
 
@@ -123,9 +176,28 @@ void SceneHierarchyPanel::DrawEntityNode(Entity entity)
 		m_SelectedEntity = entity;
 	}
 
+	bool entityDeleted = false;
+	if (ImGui::BeginPopupContextItem())
+	{
+		if (ImGui::MenuItem("Deleted Entity"))
+		{
+			entityDeleted = true;
+		}
+		ImGui::EndPopup();
+	}
+
 	if (opened)
 	{
 		ImGui::TreePop();
+	}
+
+	if (entityDeleted)
+	{
+		m_Scene->DestroyEntity(entity);
+		if (m_SelectedEntity == entity)
+		{
+			m_SelectedEntity = {};
+		}
 	}
 }
 
@@ -135,15 +207,20 @@ void SceneHierarchyPanel::DrawEntityProperties(Entity entity)
 	{
 		if (entity.HasComponent<TagComponent>())
 		{
+			auto io = ImGui::GetIO();
+			auto boldFont = io.Fonts->Fonts[0];
+			
 			auto& tag = entity.GetComponent<TagComponent>()->tag;
 			char buffer[256];
 			memset(buffer, 0, sizeof(buffer));
 			strcpy_s(buffer, tag.c_str());
 
+			ImGui::PushFont(boldFont);
 			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
 			{
 				tag = std::string(buffer);
 			}
+			ImGui::PopFont();
 		}
 
 		DrawComponent<Transform>("Transform", entity, [](auto& component)
@@ -154,12 +231,11 @@ void SceneHierarchyPanel::DrawEntityProperties(Entity entity)
 			DrawVec3Control("Scale", transform->scale, 1.0f);
 		});
 
-		DrawComponent<SpriteRender>("Transform", entity, [](auto& component)
+		DrawComponent<SpriteRender>("Sprite Renderer", entity, [](auto& component)
 		{
 			auto spriteRender = (SpriteRender*)component;
 			ImGui::ColorEdit4("Color", glm::value_ptr(spriteRender->color));
 		});
-
 	}
 	
 	
