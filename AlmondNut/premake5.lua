c2c7b74d9635bbfcd68c1bbdac3514c572f2ab0a
Huzaifa Ahmed <huzaifaahmed12@gmail.com>
2021-10-10 02:02:24 +0100

    Added premake5 file

---
+++ AlmondNut/premake5.lua
@@ -0,0 +1,62 @@
+project "AlmondNut"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	libdirs { "%{wks.location}/Almond/dependencies/lib/%{cfg.buildcfg}"}
+
+	files
+	{
+		
+		"src/**.h",
+		"src/**.cpp",
+        
+		"vendor/stb/**.h",
+		"vendor/stb/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{wks.location}/Almond/src",
+		"%{wks.location}/Almond/vendor",
+		"%{IncludeDir.SDL2}",
+		"%{IncludeDir.GL}",
+		"%{IncludeDir.glm}",
+		"%{IncludeDir.Box2D}",
+		"%{IncludeDir.ImGui}"
+	}
+
+	links
+	{
+		"Almond",
+		"SDL2",
+		"SDL2main",
+		"glew32s",
+		"glu32",
+		"opengl32",
+		"ImGui",
+		"box2d"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"ALM_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ALM_DEBUG"
+		runtime "Debug"
+		symbols "on"
+		
+		
+	filter "configurations:Release"
+		defines "ALM_RELEASE"
+		runtime "Release"
+		optimize "on"
--- premake5.lua
+++ premake5.lua
@@ -15,14 +15,16 @@ workspace "Almond"
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 IncludeDir = {}
-IncludeDir["ImGui"] = "Almond/vendor/imgui"
-IncludeDir["SDL2"] = "Almond/dependencies/include/SDL"
-IncludeDir["GL"] = "Almond/dependencies/include/GL"
-IncludeDir["Box2D"] = "Almond/dependencies/include/box2d"
-IncludeDir["stb"] = "Almond/vendor/stb"
-IncludeDir["glm"] = "Almond/vendor/glm"
+IncludeDir["ImGui"] = "%{wks.location}/Almond/vendor/imgui"
+IncludeDir["SDL2"] = "%{wks.location}/Almond/dependencies/include/SDL"
+IncludeDir["GL"] = "%{wks.location}/Almond/dependencies/include/GL"
+IncludeDir["Box2D"] = "%{wks.location}/Almond/dependencies/include/box2d"
+IncludeDir["stb"] = "%{wks.location}/Almond/vendor/stb"
+IncludeDir["glm"] = "%{wks.location}/Almond/vendor/glm"
 
 include "Almond/vendor/imgui"
+include "Almond/vendor/yaml-cpp"
+include "AlmondNut"
 
 project "Almond"
 	location "Almond"
@@ -87,69 +89,3 @@ project "Almond"
 		defines "ALM_RELEASE"
 		runtime "Release"
 		optimize "on"
-
-
-
-project "AlmondNut"
-	location "AlmondNut"
-	kind "ConsoleApp"
-	language "C++"
-	cppdialect "C++17"
-	staticruntime "off"
-
-	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
-
-	libdirs { "Almond/dependencies/lib/%{cfg.buildcfg}"}
-
-	files
-	{
-		
-		"AlmondNut/src/**.h",
-		"AlmondNut/src/**.cpp",
-		"AlmondNut/vendor/stb/**.h",
-		"AlmondNut/vendor/stb/**.cpp"
-	}
-
-	includedirs
-	{
-		"Almond/src",
-		"Almond/vendor",
-		"%{IncludeDir.SDL2}",
-		"%{IncludeDir.GL}",
-		"%{IncludeDir.glm}",
-		"%{IncludeDir.Box2D}",
-		"%{IncludeDir.ImGui}"
-	}
-
-	links
-	{
-		"Almond",
-		"SDL2",
-		"SDL2main",
-		"glew32s",
-		"glu32",
-		"opengl32",
-		"ImGui",
-		"box2d"
-	}
-
-	filter "system:windows"
-		systemversion "latest"
-
-		defines
-		{
-			"ALM_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "ALM_DEBUG"
-		runtime "Debug"
-		symbols "on"
-		
-		
-	filter "configurations:Release"
-		defines "ALM_RELEASE"
-		runtime "Release"
-		optimize "on"
-
