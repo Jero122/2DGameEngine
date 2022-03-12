9062968ee01d0e3c743aab77f103eb3d323068b8
Jero122 <huzaifaahmed12@gmail.com>
2022-03-12 16:49:38 +0000

    Post Build process of copying dll into build directory

--- GenerateProjects.bat
+++ GenerateProjects.bat
@@ -1,2 +1,7 @@
 call premake5.exe vs2019
+if not exist "bin" mkdir "bin\Debug-windows-x86_64\AlmondNut"
+if not exist "bin\Release-windows-x86_64" mkdir "bin\Release-windows-x86_64\AlmondNut"
+
+copy SDL2.dll bin\Debug-windows-x86_64\AlmondNut
+copy SDL2.dll bin\Release-windows-x86_64\AlmondNut
 PAUSE
